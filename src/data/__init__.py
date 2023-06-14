799111b2b5bc0c1151102042ace4a6a621e209df
marciotex <marciotex@gmail.com>
2023-06-13 23:40:11 -0300

    já roda com python src/data/__init__.py !

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-import click
+#import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+#from dotenv import find_dotenv, load_dotenv
 import subprocess
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+#@click.command()
+#@click.argument('input_filepath', type=click.Path(exists=True))
+#@click.argument('output_filepath', type=click.Path())
+#def main(input_filepath, output_filepath):
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     spark_submit_str= "spark-submit \
     --master local[4] \
-    MakeDatasetApp.py"
+    src/data/MakeDatasetApp.py"
     process=subprocess.Popen(spark_submit_str,stdout=subprocess.PIPE,stderr=subprocess.PIPE, universal_newlines=True, shell=True)
     stdout,stderr = process.communicate()
     if process.returncode !=0:
@@ -36,6 +37,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+ #   load_dotenv(find_dotenv())
 
     main()
